79455f136e149917aa357d04dd2a6b181502ab8d
shaurya952 <93018427+shaurya952@users.noreply.github.com>
2022-05-08 11:52:50 -0500

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,16 @@
+score=0
+function updateScore()
+{
+  score=score+1
+    document.getElementById("score").innerHTML="score :"+score;
+}
+function saveScore()
+{
+localStorage.setItem("score",score);
+}
+function nextPage()
+{
+    window.location="https://www.youtube.com/";
+    
+
+}
